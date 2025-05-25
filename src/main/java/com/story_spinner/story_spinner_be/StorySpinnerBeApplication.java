2825b1ab8f333ef4dce5979ff0ed7b0bbd3c19a7
JoseG777 <josegc7773@gmail.com>
2025-05-25 11:24:57 -0400

    initial springboot set up

---
+++ src/main/java/com/story_spinner/story_spinner_be/StorySpinnerBeApplication.java
@@ -0,0 +1,13 @@
+package com.story_spinner.story_spinner_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorySpinnerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorySpinnerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/story_spinner/story_spinner_be/StorySpinnerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.story_spinner.story_spinner_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorySpinnerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
